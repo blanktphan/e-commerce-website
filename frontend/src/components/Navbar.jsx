baaac8baaada9c313f04fa2d7e4626b0805a3dbb
blanktphan <blanktphan@gmail.com>
2025-07-21 17:40:04 +0700

    add: Create Pages and Routing.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center py5 font - font-medium'>
+
+    </div>
+  )
+}
+
+export default Navbar
